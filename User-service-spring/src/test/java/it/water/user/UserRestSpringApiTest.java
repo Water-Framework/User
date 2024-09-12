7cb08572f97c7561cf050e832ba0cd00e8b4819a
Aristide <aristide.cittadino@acsoftware.it>
2024-09-12 18:43:38 +0200

    adding logic to skip jwt validation if enabled

--- User-service-spring/src/test/java/it/water/user/UserRestSpringApiTest.java
+++ User-service-spring/src/test/java/it/water/user/UserRestSpringApiTest.java
@@ -3,9 +3,13 @@ package it.water.user;
 
 import com.intuit.karate.junit5.Karate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = UserApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(properties = {
+        "water.rest.security.jwt.validate=false"
+})
 public class UserRestSpringApiTest {
 
     @Karate.Test
